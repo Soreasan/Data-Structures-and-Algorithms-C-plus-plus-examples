7f4708496d07e630d4dc83d10a632eafde97e6c6
Kenneth <soreasan@gmail.com>
2015-08-23 17:44:43 -0600

    Created initial files in Visual Studio 2013
    Created the intial C++ files that will become the linked list.  Right
    now it's just a Hello World program.

---
+++ LinkedList/LinkedList/LinkedList.cpp
@@ -0,0 +1,19 @@
+/*
+Kenneth Adair
+www.cslearner.com
+This will be a sample single linked list example for Github.
+*/
+
+//Input and output stream
+#include <iostream>
+//Used for _getch(); (get character) so the program doesn't close right as I run it
+#include <conio.h>
+//I no longer have to type out std::cout << "Hello";, instead I type cout << "hello";
+using namespace std;
+
+int main(){
+
+	cout << "This will be my singly linked list example on Github" << endl;
+	_getch();
+	return 0;
+}
