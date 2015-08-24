618875bc09e728a7984c1e050b9840f751b3e843
Kenneth <soreasan@gmail.com>
2015-08-23 18:00:48 -0600

    Created the node structure
    For this commit I created the "node" structure which contains an integer
    called data.  Additionally  it contains a pointer to the next node.  In
    the "main" I put the syntax of interacting with the node such as
    creating a new node, updating the data, and creating and updating the
    pointer.

--- LinkedList/LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList/LinkedList.cpp
@@ -2,6 +2,9 @@
 Kenneth Adair
 www.cslearner.com
 This will be a sample single linked list example for Github.
+
+This specific commit contains the creation of the "node" struct which is what our linked list will interact with.
+Additionally in the "main" it contains sample syntax that we will be using as we interact with our node struct in the Linked List class.  
 */
 
 //Input and output stream
@@ -11,7 +14,30 @@ This will be a sample single linked list example for Github.
 //I no longer have to type out std::cout << "Hello";, instead I type cout << "hello";
 using namespace std;
 
+//Data structures interact with the "node" structure which contains a piece of information or data and pointers that point to other nodes.
+struct node{
+public:
+	int data;
+	node* next;
+};
+
 int main(){
+
+	//Created a new node similar to how we create other variables.
+	node sample;
+
+	//Updated the node's data to equal 10
+	sample.data = 10;
+
+	//Created a pointer that points to nodes. 
+	node* pointer;
+
+	//Assigned the pointer that points to the address of our sample node
+	pointer = &sample;
+
+	//Updated the node's "next" pointer to be equal to the "pointer" pointer.  
+	//Right now it just points to itself but this demonstrates the syntax of interacting with the node.
+	sample.next = pointer;
 
 	cout << "This will be my singly linked list example on Github" << endl;
 	_getch();
