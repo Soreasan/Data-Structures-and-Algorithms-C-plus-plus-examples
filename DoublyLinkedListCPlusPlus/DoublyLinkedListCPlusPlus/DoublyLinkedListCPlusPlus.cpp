224799312b63eab583581a46d396cf586493aa1e
Kenneth <soreasan@gmail.com>
2015-09-04 19:36:16 -0600

    Refined the Code a bit
    Mostly just refined the code.

--- DoublyLinkedListCPlusPlus/DoublyLinkedListCPlusPlus/DoublyLinkedListCPlusPlus.cpp
+++ DoublyLinkedListCPlusPlus/DoublyLinkedListCPlusPlus/DoublyLinkedListCPlusPlus.cpp
@@ -19,6 +19,45 @@ struct node{
 	node* prev;
 };
 
+template<typename T>
+class doublyLinkedList{
+public:
+		doublyLinkedList();	//constructor 
+	 	~doublyLinkedList();	//destructor 
+
+		//Add nodes to the linked list 
+		void addFront(const T& value);	//Adds a node to the front of the linked list 
+	 	void addEnd(const T& value);	//Adds a node to the end of the linked list 
+	 	void addAt(const int position, const T& value);	//Adds a node to a specific place in the linked list 
+	 	
+		//Returns the value of a specific node 
+		T displayFront();
+	 	T displayEnd();
+	 	T displayValueAt(const int position);	//returns the value from a specific node at a specific position in the linked list 
+	 	T operator[](const int position);		//this overloads the square brackets.  The method/behavior is identical to the displayValueAt method 
+		
+		//Deletes values 
+	 	void removeFront();
+	 	void removeEnd();
+	 	void removeAt(int position);	//removeAt removes a node from a specific point 
+	
+
+		 //Checks linked list to see if it contains a specific value 
+		 bool checkFor(T value);	//Checks the linked list to see if it contains a specific value 
+	
+
+		 //Displays all contents of the linked list 
+		 void enumerateForward();	//outputs all the contents of the linked list using pointers starting in the front
+		 void enumerateBackwards();	//outputs all the contents of the linked list starting at the back
+	
+
+private:
+		node<T>* front;	//this pointer points to the node at the front 
+		node<T>* end;	//this pointer points to the last node 
+		int count;		//This tracks how many nodes are in our linked list 
+
+};
+
 int main(){
 	cout << "This will be a doubly linked list" << endl;
 	_getch();
